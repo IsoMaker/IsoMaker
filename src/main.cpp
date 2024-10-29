4a65130d99ebd0160c96cf0dfb0686bd7d726c1e
Léa GUILLEMARD <lea.guillemard@epitech.eu>
2024-10-30 08:32:07 +0900

    build: add raylib building/installation and a code base to open a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "raylib.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
