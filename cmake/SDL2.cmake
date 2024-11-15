b801b9eaa2b7ddd297f582366d4a4ee95082ebe1
Léa GUILLEMARD <lea.guillemard@epitech.eu>
2024-11-15 12:49:48 +0900

    build: fix SDL2 installation

--- cmake/SDL2.cmake
+++ cmake/SDL2.cmake
@@ -1,12 +1,12 @@
-message(STATUS "Install SDL2 PACKAGES...")
+message(STATUS "Installing SDL2...")
 
 find_package(SDL2 CONFIG QUIET)
 
 if (NOT SDL2_FOUND)
     include(FetchContent)
     FetchContent_Declare(
-            SDL2
-            URL https://github.com/libsdl-org/SDL/releases/download/release-2.28.4/SDL2-2.28.4.tar.gz
+        SDL2
+        URL https://github.com/libsdl-org/SDL/archive/refs/tags/release-2.30.9.tar.gz
     )
     FetchContent_MakeAvailable(SDL2)
 endif()
