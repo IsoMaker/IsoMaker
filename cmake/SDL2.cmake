29344af4993124129725f480d1a013fe4cdbf391
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-11-16 19:47:37 +0900

    fix: try to modifie the SDL2 cmake

--- cmake/SDL2.cmake
+++ cmake/SDL2.cmake
@@ -1,12 +1,19 @@
-message(STATUS "Installing SDL2...")
+message(STATUS "Install SDL2 PACKAGES...")
 
-find_package(SDL2 2.0 QUIET)
+find_package(SDL2 CONFIG QUIET)
 
 if (NOT SDL2_FOUND)
     include(FetchContent)
     FetchContent_Declare(
-        SDL2
-        URL https://github.com/libsdl-org/SDL/archive/refs/tags/release-2.30.9.tar.gz
+            SDL2
+            URL https://github.com/libsdl-org/SDL/releases/download/release-2.28.4/SDL2-2.28.4.tar.gz
     )
     FetchContent_GetProperties(SDL2)
+    if (NOT SDL2_POPULATED)
+        set(FETCHCONTENT_QUIET NO)
+        FetchContent_Populate(SDL2)
+        set(SDL_STATIC OFF CACHE BOOL "" FORCE) # Utilise la version dynamique
+        set(SDL_SHARED ON CACHE BOOL "" FORCE)
+        add_subdirectory(${SDL2_SOURCE_DIR} ${SDL2_BINARY_DIR})
+    endif()
 endif()
