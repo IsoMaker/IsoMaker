47b04f03a46c7d7c10c8f730cb563779dadbf27a
Aether <lilou.stoll-metz@epitech.eu>
2024-12-03 12:57:15 +0100

    fix: rebase with the inputs and change all defines to pragma once

--- src/Render/Camera.cpp
+++ src/Render/Camera.cpp
@@ -2,62 +2,73 @@
 
 using namespace Utilities;
 
-Render::Camera::Camera() {
-    //_cameraAngles = {
-    //{0, Vector2D(1, 1)},
-    //{90, Vector2D(1, -1)},
-    //{180, Vector2D(-1, -1)},
-    //{270, Vector2D(-1, 1)},
-    //{-90, Vector2D(-1, 1)},
-    //};
+Render::Camera::Camera()
+{
+    _cameraAngles = {
+    {0, Vector2D(1, 1)},
+    {90, Vector2D(1, -1)},
+    {180, Vector2D(-1, -1)},
+    {270, Vector2D(-1, 1)},
+    {-90, Vector2D(-1, 1)},
+    };
 
-    _camera.position = Vector3D(10, 10, 10).convert();
-    _camera.target = Vector3D(0, 0, 0).convert();
+    _position = Vector3D(1, 1, 1);
+    _target = Vector3D(0, 0, 0);
     _camera.up = Vector3D(0, 1, 0).convert();
     _camera.fovy = 55;
     _camera.projection = CAMERA_ORTHOGRAPHIC;
-    //updateCamera();
+    updateCamera();
 }
 
-Render::Camera::~Camera() {
+Render::Camera::~Camera()
+{
 }
 
-void Render::Camera::start3D() {
+void Render::Camera::start3D()
+{
     BeginMode3D(_camera);
 }
 
-void Render::Camera::end3D() {
+void Render::Camera::end3D()
+{
     EndMode3D();
 }
 
-void Render::Camera::setTarget(Vector3D target) {
-    //_target = target;
+void Render::Camera::setTarget(Vector3D target)
+{
+    _target = target;
 }
 
 
-void Render::Camera::setDistance(float distance) {
-    //_distance = distance;
+void Render::Camera::setDistance(float distance)
+{
+    _distance = distance;
 }
 
-void Render::Camera::setHeight(float height) {
-    //_height = height;
+void Render::Camera::setHeight(float height)
+{
+    _height = height;
 }
 
-void Render::Camera::rotate() {
-    //_angle += 90;
-    //if (_angle > 270)
-    //    _angle = 0;
+void Render::Camera::rotate()
+{
+    _angle += 90;
+    if (_angle > 270)
+        _angle = 0;
 }
 
-void Render::Camera::rotate(float angle) {
-    //if (_cameraAngles.find(angle) != _cameraAngles.end())
-    //    _angle = angle;
+void Render::Camera::rotate(float angle)
+{
+    if (_cameraAngles.find(angle) != _cameraAngles.end())
+        _angle = angle;
 }
 
-void Render::Camera::updateCamera() {
-    //Vector2D angleModifier = _cameraAngles[_angle];
-    //_position = Vector3D(_target.x + _distance, _target.y + _height, _target.z + _distance);
-    //_position = _position * angleModifier;
-    //_camera.target = _target.convert();
-    //_camera.position = _position.convert();
+void Render::Camera::updateCamera()
+{
+    Vector2D angleModifier = _cameraAngles[_angle];
+    _position = Vector3D(_target.x + _distance, _target.y + _height, _target.z + _distance);
+    _position = _position * angleModifier;
+    _camera.target = _target.convert();
+    _camera.position = _position.convert();
 }
+
--- src/Utilities/ObjectBox.hpp
+++ src/Utilities/ObjectBox.hpp
@@ -5,32 +5,54 @@
 
 namespace Utilities {
 
-    class ObjectBox2D {
-        public:
-            ObjectBox2D();
-            ObjectBox2D(Vector2D, Vector2D, float scale = 1);
-            ObjectBox2D(const ObjectBox2D&);
-            ~ObjectBox2D();
+class ObjectBox2D
+{
+    public:
+        ObjectBox2D();
+        ObjectBox2D(Vector2D, Vector2D, float scale = 1);
+        ObjectBox2D(const ObjectBox2D&);
+        ~ObjectBox2D();
 
-            Vector2D position;
-            Vector2D assetDimensions;
-            float scale;
-    };
+        Vector2D getPosition() const;
+        Vector2D getDimensions() const;
+        Vector2D getAssetDimensions() const;
+        float getScale() const;
 
-    class ObjectBox3D {
-        public:
-            ObjectBox3D();
-            ObjectBox3D(Vector3D, Vector3D, float scale = 1);
-            ObjectBox3D(const ObjectBox3D&);
-            ~ObjectBox3D();
+        void setPosition(Vector2D);
+        void setAssetDimensions(Vector2D);
+        void setScale(float);
 
-            BoundingBox convert();
-            void ObjectBox3D::setAssetDimensionsFromBounding(BoundingBox);
+    private:
+        Vector2D _position;
+        Vector2D _assetDimensions;
+        float _scale;
+};
 
-            Vector3D position;
-            Vector3D assetDimensions;
-            float scale;
+class ObjectBox3D
+{
+    public:
+        ObjectBox3D();
+        ObjectBox3D(Vector3D, Vector3D, float scale = 1);
+        ObjectBox3D(const ObjectBox3D&);
+        ~ObjectBox3D();
 
-    };
+        Vector3D getPosition() const;
+        Vector3D getDimensions() const;
+        Vector3D getAssetDimensions() const;
+        float getScale() const;
+
+        void setPosition(Vector3D);
+        void setAssetDimensions(Vector3D);
+        void setAssetDimensionsFromBounding(BoundingBox);
+        void setScale(float);
+
+        BoundingBox convert();
+
+    private:
+        Vector3D _position;
+        Vector3D _assetDimensions;
+        float _scale;
+
+};
 
 }
